10ab9aa1ca2fc4d6766159e687a6345704e869e5
Neema Sadry <snake117@gmail.com>
2020-08-06 11:12:31 -0400

    Added Cloudinary's Product Gallery viewer to accessories show.html.erb page. Need to configure Uploaders/Cloudinary to upload to correct directory and tags.

--- db/seeds/seed_partials/accessories.rb
+++ db/seeds/seed_partials/accessories.rb
@@ -1,11 +1,10 @@
 require 'csv'
-require 'active_support/core_ext/hash'
 
 Accessory.destroy_all
 
 brand_entry = Brand.find_by(name: "Jade Black")
 user_entry = User.find_by(email: "jzuniga@gmail.com")
-uploader = ProductImageUploader.new(:store)
+#uploader = ProductImageUploader.new(:store)
 counter = 1
 
 jade_black_csv_text = File.read(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Crawl-Run)---2020-03-28T163728Z.csv'))
@@ -25,38 +24,29 @@ csv.each do |row|
   t.product_url = row['url']
   t.user_id = user_entry.id
 
-  # image1_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image1_filename']))
-  # image2_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image2_filename']))
-  # imageDemo_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['imageDemo_filename']))
+  puts "\n\n"
+  puts "\t\t --- Begin: Upload associated product images ---"
+  puts "\n\n"
 
   image1_path = { product_image: File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image1_filename'])), user: user_entry, brand: brand_entry }
   image2_path = { product_image: File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image2_filename'])), user: user_entry, brand: brand_entry }
   imageDemo_path = { product_image: File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['imageDemo_filename'])), user: user_entry, brand: brand_entry }
 
-  # product_image1_data = uploader.upload(image1_path).to_json
-  # product_image2_data = uploader.upload(image2_path).to_json
-  # product_imageDemo_data = uploader.upload(imageDemo_path).to_json
-
   product_images_attributes_array = [image1_path, image2_path, imageDemo_path]
 
   t.product_images_attributes = product_images_attributes_array
 
-  # ### Begin: ActiveStorage ###
-  # image1_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image1_filename']))
-  # image2_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image2_filename']))
-  # imageDemo_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['imageDemo_filename']))
-  # image1 = { io: image1_path, filename: File.basename(image1_path), content_type: ((File.extname(image1_path) == ".jpg" || File.extname(image1_path) == ".jpeg") ? "image/jpg" : "image/png") }
-  # image2 = { io: image2_path, filename: File.basename(image2_path), content_type: ((File.extname(image2_path) == ".jpg" || File.extname(image2_path) == ".jpeg") ? "image/jpg" : "image/png") }
-  # imageDemo = { io: imageDemo_path, filename: File.basename(imageDemo_path), content_type: ((File.extname(imageDemo_path) == ".jpg" || File.extname(imageDemo_path) == ".jpeg") ? "image/jpg" : "image/png") }
 
-  # t.images.attach([image1, image2, imageDemo])
-  # ### End: ActiveStorage ###
 
-	t.save! # Use bang to return ActiveRecord validation errors
+  puts "\n\n"
+  puts "\t\t --- End: Upload associated product images ---"
+  puts "\n\n"
+
+	t.save!
+
 	puts "#{counter} - #{t.name} by Jade Black saved.\n\n"
-  # image1_path.close
-  # image2_path.close
-  # imageDemo_path.close
+
+  counter += 1 # Keep track of how many products added and/or left to create (25 objects total)
 end
 
 
@@ -66,8 +56,25 @@ end
 puts "\n\t--- RESULTS ---\t\n"
 
 puts "There are now #{Accessory.count} rows in the accessories table!"
-counter += 1
+
 Accessory.reindex
 puts "Accessory has been reindexed!"
+
 puts "---------------- End: Accessory ----------------\n"
+
+
+# image1_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image1_filename']))
+# image2_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image2_filename']))
+# imageDemo_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['imageDemo_filename']))
+
+# ### Begin: ActiveStorage ###
+# image1_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image1_filename']))
+# image2_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['image2_filename']))
+# imageDemo_path = File.open(Rails.root.join('db', 'seeds', 'Jade_Black', 'Mens', 'Jade-Black-(Images-and-Files)---2020-03-28T163728Z', row['imageDemo_filename']))
+# image1 = { io: image1_path, filename: File.basename(image1_path), content_type: ((File.extname(image1_path) == ".jpg" || File.extname(image1_path) == ".jpeg") ? "image/jpg" : "image/png") }
+# image2 = { io: image2_path, filename: File.basename(image2_path), content_type: ((File.extname(image2_path) == ".jpg" || File.extname(image2_path) == ".jpeg") ? "image/jpg" : "image/png") }
+# imageDemo = { io: imageDemo_path, filename: File.basename(imageDemo_path), content_type: ((File.extname(imageDemo_path) == ".jpg" || File.extname(imageDemo_path) == ".jpeg") ? "image/jpg" : "image/png") }
+
+# t.images.attach([image1, image2, imageDemo])
+# ### End: ActiveStorage ###
 
